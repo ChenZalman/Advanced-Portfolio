27b09be7283867042e36f91c9eda16d14ef7b610
Chen <chenzalman96@gmail.com>
2025-09-09 15:50:30 +0300

    Fixed another problem with responsive warning

--- src/components/projects.js
+++ src/components/projects.js
@@ -58,32 +58,7 @@ export default function Projects() {
   renderArrowsWhenDisabled={false}
   renderButtonGroupOutside={false}
   renderDotsOutside={false}
-  responsive={{
-    desktop: {
-      breakpoint: {
-        max: 3000,
-        min: 1024
-      },
-      items: 3,
-      partialVisibilityGutter: 40
-    },
-    mobile: {
-      breakpoint: {
-        max: 464,
-        min: 0
-      },
-      items: 1,
-      partialVisibilityGutter: 30
-    },
-    tablet: {
-      breakpoint: {
-        max: 1024,
-        min: 464
-      },
-      items: 2,
-      partialVisibilityGutter: 30
-    }
-  }}
+  responsive={responsive}
   rewind={false}
   rewindWithAnimation={false}
   rtl={false}
